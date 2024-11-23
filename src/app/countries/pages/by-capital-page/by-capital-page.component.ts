3103be42040d19d5f11d72ac87531e8d05271f72
Christian John jiménez Calvo <christianjjc@gmail.com>
2024-11-23 16:05:40 -0500

    seccion-09-ep-09-countries-routing-module

---
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-by-capital-page',
+  standalone: false,
+  templateUrl: './by-capital-page.component.html',
+  styleUrl: './by-capital-page.component.css',
+})
+export class ByCapitalPageComponent {}
---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-sidebar',
+  standalone: false,
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css',
+})
+export class SidebarComponent {}
